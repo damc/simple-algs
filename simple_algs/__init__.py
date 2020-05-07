bfb23d58470c87327e8ebd922e3bb0fa5dc9eb17
Damian Czapiewski <damczap13245@gmail.com>
2020-05-07 15:10:05 +0100

    Change ControlStructure to WhileLoop

--- simple_algs/__init__.py
+++ simple_algs/__init__.py
@@ -3,4 +3,4 @@ from .memory import MemoryCollection, Tape, Calculator, MemoryAbstract
 from .strategy import Strategy, OperationsCounter
 from .instructions import Action, Condition, ControlStructure
 from .strategy_generators import BruteForceGenerator
-from .control_structures import ConditionalStatement, ControlStructure
+from .control_structures import ConditionalStatement, WhileLoop
